694efe04a8e39dcf2e923a9118ccc33179551b02
nitish-11k <nitishk0014@gmail.com>
2026-01-03 14:45:15 +0530

    Feat : Sorting Method -> Selection Sort and Insertion Sort implementation

--- src/DSA_2026/SORTING/InsertionSort.java
+++ src/DSA_2026/SORTING/InsertionSort.java
@@ -1,4 +1,33 @@
 package DSA_2026.SORTING;
 
 public class InsertionSort {
+
+    static void sort(int [] arr){
+        for(int i =1 ; i < arr.length; i++){
+            /* We are assuming that first element in an array is already sorted that's why
+                we start iterating from the second element
+                1 to arr.length
+             */
+            int key = arr[i];
+            int j = i - 1;
+
+            while( j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j= j -1;
+            }
+            arr[j+1]= key;
+        }
+    }
+
+    static void print(int [] arr){
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+
+    public static void main (String [] args){
+        int [] arr = {2, 5, 4, 1, 7, 6, 3};
+        sort(arr);
+        print(arr);
+    }
 }
--- src/DSA_2026/SORTING/SelectionSorting.java
+++ src/DSA_2026/SORTING/SelectionSorting.java
@@ -1,4 +1,51 @@
 package DSA_2026.SORTING;
 
 public class SelectionSorting {
+
+    /*
+    Selection Sort is a comparison-based sorting algorithm.
+    It sorts by repeatedly selecting the smallest (or largest) element.
+
+    Find the smallest element and swap it with the first element.
+    This way we get the smallest element at its correct position.
+    Then find the smallest among remaining elements (or second smallest) and
+    swap it with the second element.
+
+    Time Complexity O(n^2)
+    Space Complexity is O(1) because it do in-place sorting not require any extra space
+     */
+
+    static void sortTechnique(int [] arr){
+        int n = arr.length;
+
+        for(int i = 0; i < n-1; i++){
+
+            int min_index = i;
+
+            for(int j = i+1; j <  n; j++){
+
+                if(arr[j] < arr[min_index]){
+
+                    min_index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index]= temp;
+        }
+    }
+
+    static void print(int [] arr){
+        for(int ele : arr){
+            System.out.print(ele  + " ");
+        }
+    }
+
+    public static void main(String [] args){
+
+        int [] arr= {3, 2, 6, 1, 9, 7, 4, 5, 8};
+
+        sortTechnique(arr);
+        print(arr);
+    }
 }
